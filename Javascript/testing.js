2187e31a6822fa603f3509d44b1c08ad45601714
MUHAMMAD HAFIZUL BIN AHMAD HUSNI <116468286+hafizulhusni@users.noreply.github.com>
2024-09-25 19:04:58 +0800

    Add files via upload

---
+++ Javascript/testing.js
@@ -0,0 +1,32 @@
+
+
+
+
+var myArray = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [[10, 11, 12], 13, 14]];
+    
+var myData = myArray[2][1];
+
+
+//manipulate arrays with push()
+
+var ourArray = ["Pijoy", "Balqis", "Sofea"];
+ourArray.push(["happy", "joy"]);
+
+//new setup
+
+var meArray = [["JJ", 23], ["cat", 69]];
+meArray.push(["dog", 99]);
+
+// MANIPULATE ARRAYS WITH pop()
+
+var aArray = [1, 2, 3];
+var removedArray = aArray.pop();
+
+//SETUP EXAMPLE
+
+var bArray = [["John", 23], ["cat", 1]];
+
+var removedBArray = bArray.pop();
+
+//MANIPULATE ARRAYS WITH shift()s
+
